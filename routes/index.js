3d83ea089cfb7f30b200a1e31d278b9ea2702361
Don Casanova <ethanjuan@yahoo.com>
2019-02-14 07:23:50 -0600

    test

--- routes/index.js
+++ routes/index.js
@@ -100,7 +100,7 @@ router.get('/', (req, res) => {
     var twoDigitMonth = ((fullDate.getMonth().length + 1) === 1) ? (fullDate.getMonth() + 1) : '0' + (fullDate.getMonth() + 1);
     var twoDigitDate = ((fullDate.getDate().length + 1) === 1) ? (fullDate.getDate() + 1) : '0' + (fullDate.getDate());
     var currentDate = fullDate.getFullYear() + "-" + twoDigitMonth + "-" + twoDigitDate;
-   
+    console.log("this is the date " + twoDigitDate);
     
     Upload.find({ date: { $gte: currentDate } }).sort({ date: 1 }).exec(function (err, uploadsResults) {
         //loads a dummy object in the event there are no events
